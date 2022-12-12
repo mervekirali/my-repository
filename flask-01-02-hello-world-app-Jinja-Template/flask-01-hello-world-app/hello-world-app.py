d65719c8acc93fa05cfebaa59bfd3382becc76e7
mervekirali <merve@kirali.net>
2022-12-12 01:02:31 -0500

    little modification of the flask files

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #Create a flask web app
 
-@app.route('/')
-def hello():
+
+#A decorator is a design pattern in Python that allows a user to add new functionality to an existing object without modifying its structure.
+
+@app.route('/')       #Assign a URL route the hello function with decorator @app.route('/')
+def hello():          #Create a function called hello and return hello world string
 
     return "Hello World"
 
@@ -18,11 +21,11 @@ def third():
     return "This is the subpage of the third page"
 
 
-@app.route('/forth/<string:id>')
+@app.route('/forth/<string:id>')   #Create a dynamic URL which takes id number dynamically and return with a message which show id of page.
 def fourth(id):
 
     return f"The id of this page is {id}"
 
-if __name__ == '__main__':
+if __name__ == '__main__': #the main file is hello-world-app.py, so this is the main file to be run
     app.run(debug=True)
 
--- flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template  #Import Flask and render_template modules.
+
+app = Flask(__name__) #Create a flask app
+
+#Create a function named `head` which sends number `number1` and `number2` variables to the `index.html`. 
+#Use these variables into the `index.html` file. 
+#Assign a URL route the `head` function with decorator `@app.route('/')`.
 @app.route('/')
 def head():
     return render_template('index.html', number1=10, number2=20)
-# Create a function named `number` which sends number `num1` and `num2`
+
+
+
+
+# Create a function named `number` which sends number `value1` and `value2`
 # and sum of them to the `body.html`. Use these variables into the `body.html` file.
 # Assign a URL route the `number` function with decorator `@app.route('/sum')`.
 @app.route('/sum')
@@ -11,5 +20,7 @@ def number():
     x = 15
     y = 20
     return render_template('body.html', value1=x, value2=y, sum=x+y)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
