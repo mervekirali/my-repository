ee1c7f7617e75302e4ed2a42767c3986c67ef3c8
mervekirali <merve@kirali.net>
2023-02-09 18:58:17 -0500

    initial commit for kittens carousel

--- Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
+++ Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
@@ -8,3 +8,19 @@ wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/t
 wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/result.html
 wget https://raw.githubusercontent.com/serdarcw/cli_deneme/master/app.py
 python3 app.py
+
+
+
+# #!/bin/bash -x
+# yum update -y
+# yum install python3 -y
+# pip3 install flask
+# cd /home/ec2-user
+# FOLDER="https://raw.githubusercontent.com/altazbhanji/my-repository/main/Project-001-Roman-Numerals-Converter"
+# wget ${FOLDER}/app.py
+# mkdir templates
+# cd templates
+# wget ${FOLDER}/templates/index.html
+# wget ${FOLDER}/templates/result.html
+# cd ..
+# python3 app.py
